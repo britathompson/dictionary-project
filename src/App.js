398d1e183587d669488a025dce67d937b2ecc109
britathompson <74376336+britathompson@users.noreply.github.com>
2022-10-02 19:39:24 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import detective from './detective.jpeg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={detective} className="App-logo img-fluid" alt="black and white detective logo" />
+      </header>
+    </div>
+  );
+}
+
+
